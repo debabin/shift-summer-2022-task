069332edf637dd2348448af5fc7387659f0a5ef1
Sasha-N <avgystina17@mail.ru>
2022-07-07 09:14:14 +0300

    add html-form

--- order-app/src/App.js
+++ order-app/src/App.js
@@ -2,22 +2,66 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const onSubmit = (d) => {
+    alert(JSON.stringify())
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>
+          Заявка на доставку
+        </h1>
+      </div>
+      <form onSubmit={onSubmit}>
+
+        <h2>Персональные данные получателя</h2>
+        <label>Имя</label>
+        <input name="firstName" />
+        <label>Фамилия</label>
+        <input name="secondName" />
+        <label>Отчество</label>
+        <input name="patronymic" />
+        <label>Есть отчество</label>
+        <input type="checkbox" name="checkPatronymic" />
+        <label>Дата рождения</label>
+        <input type="date" name="dateOfBirth" />
+        <label>Адрес доставки</label>
+        <input type="text" name="adress" />
+
+        <h2>Персональные данные отправителя</h2>
+        <label>Имя</label>
+        <input name="firstName" />
+        <label>Фамилия</label>
+        <input name="secondName" />
+        <label>Отчество</label>
+        <input name="patronymic" />
+        <label>Есть отчество</label>
+        <input type="checkbox" name="checkPatronymic" />
+        <label>Дата рождения</label>
+        <input type="date" name="dateOfBirth" />
+        <label>Адрес доставки</label>
+        <input type="text" name="adress" />
+
+        <h2>Адрес доставки</h2>
+        <label>Город</label>
+        <input name="city" />
+        <label>Улица</label>
+        <input name="numberOfStreet" />
+        <label>Дом</label>
+        <input name="numberOfBuilding" />
+        <label>Квартира</label>
+        <input name="numberOfFlat" />
+
+        <h2>Данные о посылке</h2>
+        <label>Тип посылки</label>
+        <input type="select" name="type" />
+        <label>Примерный вес посылки</label>
+        <input name="weight" />
+        <label>Заметка для курьера</label>
+        <input type="textarea" name="numberOfFlat" />
+
+        <input type="submit" value="submit" />
+      </form>
     </div>
   );
 }
