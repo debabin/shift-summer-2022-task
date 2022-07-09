1716f97320a2c02cd5e61ebf3de4a052a28f3c7a
Sasha-N <avgystina17@mail.ru>
2022-07-09 09:20:32 +0300

    add react-hook-library and some validation

--- order-app/src/App.js
+++ order-app/src/App.js
@@ -1,10 +1,12 @@
-import pic from './findName.png';
 import './App.css';
+import React from "react";
+import { useForm } from 'react-hook-form'
 
 function App() {
   document.body.style.backgroundColor = "black";
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = (d) => {
-    alert(JSON.stringify())
+    console.log(d)
   }
   return (
     <div>
@@ -13,53 +15,113 @@ function App() {
           Заявка на доставку
         </h2>
       </div>
-      <form className="form-box" style={{backgroundColor: '#cecece'}} onSubmit={onSubmit}>
+      <form className="form-box" style={{backgroundColor: '#cecece'}} onSubmit={handleSubmit(onSubmit)}>
+
 
         <h2>Персональные данные получателя</h2>
+
         <label>Имя</label>
-        <input name="firstName" />
+        <input {...register("firstName1", { required: true, 
+          minLength: { value: 2, message: 'Ваше имя должно содержать более 2 букв'},
+          maxLength: { value: 30, message: 'Ваше имя должно содержать не более 30 букв'}} )}/>
+        <p>{errors.firstName1?.message}</p>
+
         <label>Фамилия</label>
-        <input name="secondName" />
+        <input {...register("secondName1", { required: true, 
+          minLength: { value: 2, message: 'Ваша фамилия должна содержать более 2 букв'},
+          maxLength: { value: 30, message: 'Ваша фамилия должна содержать не более 30 букв'}} )}/>
+        <p>{errors.secondName1?.message}</p>
+
         <label>Отчество</label>
-        <input type="checkbox" name="checkPatronymic" />        
-        <input name="patronymic" />
+        <input type="checkbox" name="checkPatronymic1" />        
+        <input {...register("patronymic1", { required: true, 
+          minLength: { value: 2, message: 'Ваше отчество должно содержать более 2 букв'},
+          maxLength: { value: 30, message: 'Ваше отчество должно содержать не более 30 букв'}} )}/>
+        <p>{errors.patronymic1?.message}</p>
+
         <label>Дата рождения</label>
-        <input type="date" name="dateOfBirth" />
-        <label>Адрес доставки</label>
-        <input type="text" name="adress" />
+        <input type="date" name="dateOfBirth1" />
+
+        <label>Адрес проживания</label>
+        <input {...register("adress1", { required: true, 
+          minLength: { value: 10, message: 'Поле должно содержать более 10 символов'},
+          maxLength: { value: 50, message: 'Поле должно содержать не более 50 символов'}} )}/>
+        <p>{errors.adress1?.message}</p>
+
 
         <h2>Персональные данные отправителя</h2>
+
         <label>Имя</label>
-        <input name="firstName" />
+        <input {...register("firstName2", { required: true, 
+          minLength: { value: 2, message: 'Ваше имя должно содержать более 2 букв'},
+          maxLength: { value: 30, message: 'Ваше имя должно содержать не более 30 букв'}} )}/>
+        <p>{errors.firstName2?.message}</p>
+
         <label>Фамилия</label>
-        <input name="secondName" />
+        <input {...register("secondName2", { required: true, 
+          minLength: { value: 2, message: 'Ваша фамилия должна содержать более 2 букв'},
+          maxLength: { value: 30, message: 'Ваша фамилия должна содержать не более 30 букв'}} )}/>
+        <p>{errors.secondName2?.message}</p>
+
         <label>Отчество</label>
-        <input type="checkbox" name="checkPatronymic" />        
-        <input name="patronymic" />
+        <input type="checkbox" name="checkPatronymic2" />        
+        <input {...register("patronymic2", { required: true, 
+          minLength: { value: 2, message: 'Ваше отчество должно содержать более 2 букв'},
+          maxLength: { value: 30, message: 'Ваше отчество должно содержать не более 30 букв'}} )}/>
+        <p>{errors.patronymic2?.message}</p>
+
         <label>Дата рождения</label>
-        <input type="date" name="dateOfBirth" />
-        <label>Адрес доставки</label>
-        <input type="text" name="adress" />
+        <input type="date" name="dateOfBirth2" />
+
+        <label>Адрес проживания</label>
+        <input {...register("adress2", { required: true, 
+          minLength: { value: 10, message: 'Поле должно содержать более 10 символов'},
+          maxLength: { value: 50, message: 'Поле должно содержать не более 50 символов'}} )}/>
+        <p>{errors.adress2?.message}</p>
+
 
         <h2>Адрес доставки</h2>
+
         <label>Город</label>
-        <input name="city" />
+        <input {...register("city", { required: true, 
+          minLength: { value: 2, message: 'Поле должно содержать более 2 символов'},
+          maxLength: { value: 50, message: 'Поле должно содержать не более 50 символов'}} )}/>
+        <p>{errors.city?.message}</p>
+
         <label>Улица</label>
-        <input name="numberOfStreet" />
+        <input {...register("numberOfStreet", { required: true, 
+          minLength: { value: 2, message: 'Поле должно содержать более 2 символов'},
+          maxLength: { value: 60, message: 'Поле должно содержать не более 60 символов'}} )}/>
+        <p>{errors.numberOfStreet?.message}</p>
+
         <label>Дом</label>
-        <input name="numberOfBuilding" />
+        <input {...register("numberOfBuilding", { required: true, 
+          minLength: { value: 1, message: 'Поле должно содержать более 1 символов'},
+          maxLength: { value: 10, message: 'Поле должно содержать не более 10 символов'}} )}/>
+        <p>{errors.numberOfBuilding?.message}</p>
+
         <label>Квартира</label>
-        <input name="numberOfFlat" />
+        <input {...register("numberOfFlat", { required: true, 
+          minLength: { value: 1, message: 'Поле должно содержать более 1 символов'},
+          maxLength: { value: 10, message: 'Поле должно содержать не более 10 символов'}} )}/>
+        <p>{errors.numberOfFlat?.message}</p>
+
+        <label>Заметка для курьера</label>
+        <input type="textarea" {...register("notes1")} />
+
 
         <h2>Данные о посылке</h2>
-        <label>Тип посылки</label>
-        <input type="select" name="type" />
-        <label>Примерный вес посылки</label>
-        <input name="weight" />
-        <label>Заметка для курьера</label>
-        <input type="textarea" name="numberOfFlat" />
 
-        <input type="submit" value="submit" />
+        <label>Тип посылки</label>
+        <input type="select" {...register("type")} />
+
+        <label>Примерный вес посылки</label>
+        <input {...register("weight")} />
+
+        <label>Заметка для курьера</label>
+        <input type="textarea" {...register("numberOfFlat")} />
+
+        <input type="submit" value="Отправить" />
       </form>
     </div>
   );
