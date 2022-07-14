03505f2c4244c3b626ab2157947099172b452a69
DanilaLapatin <102025632+DanilaLapatin@users.noreply.github.com>
2022-07-14 13:42:30 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const requestURL="https://shift-summer-2022-backend.herokuapp.com/api/create/order"
+
+
+function sendRequest(method,url,body=null){
+  return new Promise((resolve,reject)=>{
+	const xhr=new XMLHttpRequest()
+    xhr.open(method,url)
+    xhr.reponseType='json'
+
+    xhr.onload=()=>{
+	  if (xhr.status>=400) {
+		reject(xhr.response)
+	  }
+	  else{
+  	    resolve(xhr.response)
+	  }
+    }
+    xhr.onerror=()=>{
+  	  console.log(xhr.response)
+    }
+    xhr.send(body)	
+  })
+}
+
+sendRequest('POST',requestURL,body)
+  .then(data=>console.log(data))
+  .catch(err=>console.log(err))
